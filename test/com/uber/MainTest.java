71b27ad3522671e12567f9d64c31b3f038bbfdc5
Chris Anatalio <anataliocs@gmail.com>
2016-01-21 18:33:14 -0500

    Create log file of test

--- test/com/uber/MainTest.java
+++ test/com/uber/MainTest.java
@@ -2,11 +2,20 @@ package com.uber;
 
 import junit.framework.TestCase;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
 public class MainTest extends TestCase {
 
     private static final Integer DEFAULT_VAL = 3;
     private static final Integer DEFAULT_VAL2 = 4;
     private static final Integer EXPECTED_RESULT = 12;
+
+    Map<String, String> results;
 
     Product classUnderTest;
 
@@ -15,6 +24,7 @@ public class MainTest extends TestCase {
         super.setUp();
 
         classUnderTest = new Product();
+        results = new HashMap<String, String>();
     }
 
     public void testProduct() throws Exception {
@@ -22,5 +32,30 @@ public class MainTest extends TestCase {
         Integer actualResult = classUnderTest.getProduct(DEFAULT_VAL, DEFAULT_VAL2);
 
         assertEquals(EXPECTED_RESULT, actualResult);
+
+        if( EXPECTED_RESULT.equals(actualResult))
+            results.put("test1", "success");
+
+    }
+
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        results.keySet().stream().forEach(
+
+                        r -> {
+                            List<String> lines = Arrays.asList(r, results.get(r));
+                            try {
+                                Files.write(Paths.get("unit-test-"+ r + ".txt"), lines, StandardCharsets.UTF_8,
+                                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+        );
+
     }
 }
